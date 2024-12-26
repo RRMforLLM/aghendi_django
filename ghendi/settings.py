2c4df060b30bd4e1d86b2bfd6a2e98f6dc8cf792
RRMforLLM <ricardoroblesm09@gmail.com>
2024-12-26 17:48:46 -0600

    Add staticfiles

--- ghendi/settings.py
+++ ghendi/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-# Specify the location of your static files (ensure it's set to the "static" directory)
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # for deployment
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
