a5b07b75c820728de48c5ddb6fa99613e8151986
RRMforLLM <ricardoroblesm09@gmail.com>
2024-12-26 18:02:33 -0600

    Add whitenoise

--- ghendi/settings.py
+++ ghendi/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ghendi.urls'
@@ -121,9 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # for deployment
+STATICFILES_STORAGE = "whitenoise. storage.CompressedManifestStaticFilesStorage"
+
+STATIC_URL = '/static/'
+
+# Specify the location of your static files (ensure it's set to the "static" directory)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = (BASE_DIR / 'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
