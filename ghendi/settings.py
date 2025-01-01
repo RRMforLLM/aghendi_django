96d01956d22abfddebadc3bf9cdd3d84083ddd8f
RRMforLLM <ricardoroblesm09@gmail.com>
2025-01-01 13:24:10 -0600

    Add missing templates

--- ghendi/settings.py
+++ ghendi/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ricardoroblesm09@gmail.com'
-EMAIL_HOST_PASSWORD = 'puuy mjcr hnuv wzvl'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
